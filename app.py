2041201872a8a777ad0c22ded44ef879bd92911c
matrobinson <mathewrobinson.27@gmail.com>
2024-12-06 15:01:03 +0000

    Added customer product status

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ class BasisOfPrep(Enum):
 	YEARLY = 3
 
 
+
+# ------------------------------------
+# Data Loading
+# ------------------------------------
+
 def _load_data(file_path: str) -> pd.DataFrame:
 	'''
 	Loads data from CSV
@@ -30,6 +35,12 @@ def _load_dim_date():
 	data['date'] = pd.to_datetime(data['date'], format="%d/%m/%Y")
 	return data
 
+
+
+
+# ------------------------------------
+# ETL - Extract Transform Load
+# ------------------------------------
 
 def _clean_data(data: pd.DataFrame, date_field_names: [str], input_date_format: str) -> pd.DataFrame:
 	'''
@@ -62,24 +73,36 @@ def _clean_data(data: pd.DataFrame, date_field_names: [str], input_date_format: 
 	end_year = 2023
 	yr_range_under_review = range(start_year, end_year + 1)
 
+	# Filter for specific range of data in period under review
 	data = data[np.isin(data['year'], yr_range_under_review)]
 
-	# Cleanse data
-	cleaned_data = _cleanse_data(data=data)
-
-	return cleaned_data
+	return data
 
 
-def _cleanse_data(data: pd.DataFrame) -> pd.DataFrame:
+
+# ------------------------------------
+# Calculations
+# ------------------------------------
+
+
+def _create_arr_data(data: pd.DataFrame) -> pd.DataFrame:
 	'''
-	Drops any NA values and removes any zero value rows
+	Creates an annual recurring revenue amount based off of the reported monthly recurring revenue 
 	'''
-	df = data.dropna()
-	row_count_before = df.shape[0]
-	df = df.loc[df['value']!=0]
-	rows_dropped = row_count_before - df.shape[0]
-	print("Rows dropped ", rows_dropped)
-	return df
+
+	# Get only the revenue values
+	recurring_rev = data[data['metric'] == 'Revenue']
+
+	# Filter for only recurring revenue
+	recurring_rev = recurring_rev[recurring_rev['revenue_type'].str.lower() == 'recurring']
+	
+	# Calculate the ARR from the MRR
+	recurring_rev['value'] = recurring_rev['value'] * 12 # Months in year
+	recurring_rev['metric'] = 'ARR' # Change metric to annual recurring revenue
+
+	# Add the the ARR data to existing data
+	data = data._append(recurring_rev)
+	return data
 
 
 
@@ -92,6 +115,84 @@ def _calculate_total_by_customer_product_by_year(data: pd.DataFrame) -> pd.DataF
 	return df
 
 
+
+
+def _apply_tagging_conditions(data: pd.DataFrame, output_status_field_name: str) -> pd.DataFrame:
+	'''
+	Applies tagging to data set based on current and previous row value
+	'''
+	
+	conditions = []
+	choices = []
+
+	# If product level data structure then apply a credit note condition first too
+	if 'product' in data.columns:
+		conditions.append(data['product'].str.lower() == 'credit notes')
+		choices.append('Credit Notes')
+
+	# Apply vectorized conditions to determine customer status
+	conditions.extend([
+		((data['value'] >= 0) & (data['prior_yr_value'] >= 0) & (data['value'] == data['prior_yr_value'])),  # Recurring
+		(data['value'] > data['prior_yr_value']),  # Upsell
+		(data['value'] < data['prior_yr_value']),  # Downsell
+		(data['value'] == 0) & (data['prior_yr_value'] > 0),  # Lost
+		(data['value'] > 0) & (data['prior_yr_value'].isna()),  # New 
+		(data['value'] < 0) | (data['prior_yr_value'] < 0), # Credit Notes
+		
+	])
+
+	choices.extend(['Recurring','Upsell', 'Downsell', 'Lost', 'New', "Credit Notes"])
+
+	# Create a field name with the status tagging
+	data[output_status_field_name] = np.select(conditions, choices, default='N/A')
+	
+	return data
+
+
+def _tag_final_cust_prod_status(data: pd.DataFrame) -> pd.DataFrame:
+	'''
+	Takes the customer status and the customer product status and 
+	creates a final status for each record in the data
+	'''
+
+	cust_status_field_name = 'cust_status_annual'
+	cust_prod_status_field_name = 'custprod_status_annual_temp'
+
+	conditions = [
+		(data[cust_prod_status_field_name].str.lower() == 'credit notes'), # Credit Notes
+
+		(data[cust_status_field_name].str.lower() == 'new'), # new
+		(data[cust_status_field_name].str.lower() == 'reactivated'), # reactivated
+		(data[cust_status_field_name].str.lower() == 'deactivated'), # deactivated
+		(data[cust_status_field_name].str.lower() == 'lost'), # lost
+
+		# Cross Sell
+		((data[cust_status_field_name].str.lower().isin(['recurring', 'upsell', 'downsell'])) &
+         (data[cust_prod_status_field_name].str.lower() == 'new')),
+
+		# Cross loss - existing product lost for existing customer
+        ((data[cust_status_field_name].str.lower().isin(['recurring', 'upsell', 'downsell'])) &
+         (data[cust_prod_status_field_name].str.lower() == 'lost')),
+
+		# Customer prod level upsell, downsell or revenue is the same 'recurring'
+		(data[cust_prod_status_field_name].str.lower() == 'upsell'),
+		(data[cust_prod_status_field_name].str.lower() == 'downsell'),
+		(data[cust_prod_status_field_name].str.lower() == 'recurring'),
+
+		# Where customer status is tagged as a credit note but the product is new
+		((data[cust_status_field_name].str.lower() == 'credit notes') & data[cust_prod_status_field_name].str.lower() == 'new'),
+		((data[cust_status_field_name].str.lower() == 'credit notes') & data[cust_prod_status_field_name].str.lower() == 'lost'),
+	]
+
+	# Match the choices with the conditions
+	choices = ['Credit Notes', 'New', 'Reactivated', 'Deactivated', 'Lost', 'xSell', 'xLoss', 'Upsell', 'Downsell', 'Recurring', 'xSell', 'xLoss']
+
+	# Apply the choices in a vectorized operation
+	data['custprod_status_annual'] = np.select(conditions, choices, default='N/A')
+
+	return data
+
+
 def _tag_customer_status(data: pd.DataFrame) -> pd.DatetimeIndex:
 	'''
 	Tags the customer level status for revenue. 
@@ -102,62 +203,74 @@ def _tag_customer_status(data: pd.DataFrame) -> pd.DatetimeIndex:
 	'''
 	
 	# Using revenue only
-	revenue_df = data[data['metric'].str.lower() =='revenue']
+	revenue_df = data[data['metric'].str.lower() == 'arr']
 
-	# Sort values so they are ordered by customer and year
-	revenue_df = revenue_df.sort_values(by=['customer', 'product', 'year']).reset_index(drop=True)
-	
 	# CUSTOMER LEVEL
+	cust_rev_df = revenue_df
+	
+	# Sort values so they are ordered by customer and year
+	cust_rev_df = cust_rev_df.sort_values(by=['customer', 'product', 'year']).reset_index(drop=True)
+
 	# Get the total revenue per customer per year
-	cust_rev_by_yr = revenue_df.groupby(['customer', 'year'])['value'].sum().reset_index()
+	cust_rev_by_yr = cust_rev_df.groupby(['customer', 'year'])['value'].sum().reset_index()
 	cust_rev_by_yr = cust_rev_by_yr.sort_values(by=['customer', 'year']).reset_index(drop=True)
 	
 	# Compare the current and previous rows for the same customer (row-1 customer and row 0 customer)
 	cust_rev_by_yr['prior_yr_value'] = cust_rev_by_yr['value'].shift(1).where(cust_rev_by_yr['customer'] == cust_rev_by_yr['customer'].shift(1), None)
 
-	print(cust_rev_by_yr.head(10))
-
 	# Tag customer and product status'
 	cust_rev_by_yr = _apply_tagging_conditions(data=cust_rev_by_yr, output_status_field_name='cust_status_annual')
 
 	# Merge 'cust_status_annual' back to the original data
-	df = data.merge(cust_rev_by_yr[['customer', 'year', 'cust_status_annual']], on=['customer', 'year'], how='left')
-	print(df.head)
+	data = data.merge(cust_rev_by_yr[['customer', 'year', 'cust_status_annual']], on=['customer', 'year'], how='left')
 
-	return df
-
-
-def _apply_tagging_conditions(data: pd.DataFrame, output_status_field_name: str) -> pd.DataFrame:
-	'''
-	Applies tagging to data set based on current and previous row value
-	'''
-	# Apply vectorized conditions to determine customer status
-	conditions = [
-		(data['value'] > 0) & (data['prior_yr_value'].isna()),  # New customer (first year)
-		(data['value'] == 0) & (data['prior_yr_value'] > 0),  # Lost customer
-		(data['value'] > data['prior_yr_value']),  # Upsell
-		(data['value'] < data['prior_yr_value']),  # Downsell
-	]
-
-	choices = ['New', 'Lost', 'Upsell', 'Downsell']
-
-	# Create a field name with the status tagging
-	data[output_status_field_name] = np.select(conditions, choices, default='Recurring')
+	# PRODUCT LEVEL
+	custprod_rev_df = revenue_df
 	
+	# Get the total revenue per customer per year
+	custprod_rev_by_yr = custprod_rev_df.groupby(['customer', 'product', 'year'])['value'].sum().reset_index()
+	custprod_rev_by_yr = custprod_rev_by_yr.sort_values(by=['customer', 'product', 'year']).reset_index(drop=True)
+	
+	# Compare the current and previous rows for the same customer (row-1 customer prod and row 0 customer prod)
+	custprod_rev_by_yr['prior_yr_value'] = custprod_rev_by_yr['value'].shift(1).where(
+		(custprod_rev_by_yr['product'] == custprod_rev_by_yr['product'].shift(1)) &
+		(custprod_rev_by_yr['customer'] == custprod_rev_by_yr['customer'].shift(1)),
+		None
+	)
+
+	custprod_rev_by_yr = _apply_tagging_conditions(data=custprod_rev_by_yr, output_status_field_name='custprod_status_annual_temp')
+
+	# Merge 'cust_status_annual' back to the original data
+	data = data.merge(custprod_rev_by_yr[['customer', 'product', 'year', 'custprod_status_annual_temp']], on=['customer', 'product', 'year'], how='left')
+
+	# Final combined customer and product status
+	data = _tag_final_cust_prod_status(data=data)
+	
+	data = data.drop(columns=['custprod_status_annual_temp'])
+
+	print(data)
+
 	return data
 
 
 
 def main():
+
 	print("Starting pipeline")
+	
 	# Todo - add user input for column names that are date fields. 
 	# As a streamlit app we can add check box's next to the column names to do this
 	date_field_names = ['date']
 	data = _load_data(file_path='data.csv')
+
 	clean_data = _clean_data(
 		data=data, 
 		date_field_names=date_field_names,
 		input_date_format="%d/%m/%Y"
+	)
+
+	clean_data = _create_arr_data(
+		data=clean_data
 	)
 
 	clean_data = _calculate_total_by_customer_product_by_year(
